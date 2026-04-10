7c61b1f753454b2b0824644abf9f02113d66d852
zalamp <844060885@qq.com>
2026-04-11 00:19:46 +0800

    再次修改

--- build/Desktop_Qt_6_5_0_MinGW_64_bit-Debug/qtcsettings.cmake
+++ build/Desktop_Qt_6_5_0_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -1,2 +1,12 @@
 # This file is managed by Qt Creator, do not edit!
 
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Qt/Qt6/6.5.0/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/Qt/Qt6/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Qt/Qt6/6.5.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "D:/Qt/Qt6/MaintenanceTool.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/Qt/Qt6/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/IOS/Downloads/War3ModelViewer/build/Desktop_Qt_6_5_0_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
